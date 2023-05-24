acd7280bf550cd34c6b3d66bfa7afbea925a98d6
vekt0R-HUB <zealot.algo@gmail.com>
2023-05-24 12:34:15 -0400

    Added costumes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar.jsx';
-import example from './assets/exampleCostume.jpg';
 import CostumeList from "./components/CostumeList.jsx";
 import SearchBar from "./components/SearchBar.jsx";
 import Footer from "./components/Footer.jsx";
 import ContactUs from "./components/ContactUs.jsx";
+import costumes from './Costumes'
 
-const costumes = [
-    {name: 'Costume 1', img: example, fitSize: 15, gender: 'M'},
-    {name: 'Costume 2', img: example, fitSize: 15, gender: 'F'},
-    {name: 'Costume 3', img: example, fitSize: 15, gender: 'F'},
-    {name: 'Costume 4', img: example, fitSize: 15, gender: 'M'},
-];
 
 function App() {
     const [searchText, setSearchText] = useState('');
---
+++ src/Costumes.jsx
@@ -0,0 +1,125 @@
+import a1 from './assets/Antifaz 2.jpg';
+import a2 from './assets/Antifaz.jpg';
+import a3 from './assets/Barbie en Paris T10.jpg';
+import a4 from './assets/Bella (con tiara).jpg';
+import a5 from './assets/Bella T10.jpg';
+import a6 from './assets/Blanca Nieves T5-6.jpg';
+import a7 from './assets/Bombero T4-5.jpg';
+import a8 from './assets/Bruja (adulto) (2).jpg';
+import a9 from './assets/Bruja (adulto) (3).jpg';
+import b1 from './assets/Bruja (adulto) (4).jpg';
+import b2 from './assets/Bruja (adulto).jpg';
+/* =================================================== */
+import b3 from './assets/Bruja TS.jpg';
+import b4 from './assets/Bruja-TS.jpg';
+import b5 from './assets/Brujita T4-5.jpg';
+import b6 from './assets/Brujita T10.jpg';
+import b7 from './assets/Brujita Verde.jpg';
+import b8 from './assets/Buddy el Vaquero T5-6.jpg';
+import b9 from './assets/Cabaret Anos 30.jpg';
+import c1 from './assets/Cangrejo (cabeza).jpg';
+import c2 from './assets/Capitan America T8.jpg';
+import c3 from './assets/Cleopatra (adulto).jpg';
+import c4 from './assets/Egipcio (adulto).jpg';
+import c5 from './assets/Elena de Avalor T8.jpg';
+import c6 from './assets/Bruja (con sombrero).jpg';
+/* ==================================================== */
+import c7 from './assets/Fiona T8-10.jpg';
+import c8 from './assets/Gata (adulto).jpg';
+import c9 from './assets/Halloween (adulto).jpg';
+import d1 from './assets/It (adulto).jpg';
+import d2 from './assets/Ladybug T4.jpg';
+import d3 from './assets/Malefica TS.jpg';
+import d4 from './assets/Mariachi.jpg';
+import d5 from './assets/Menique T4-6.jpg';
+import d6 from './assets/Mickey Mouse (adulto).jpg';
+import d7 from './assets/Mode de los 70.jpg';
+import d8 from './assets/Murcielago T6-8.jpg';
+import d9 from './assets/Nemo (cabeza).jpg';
+import e1 from './assets/Payasito T5-7.jpg';
+import e2 from './assets/Peter Pan & Menique.jpg';
+import e3 from './assets/Picapiedras (adulto).jpg';
+/* ==================================================== */
+import e4 from './assets/Pilar (Jose Marti).jpg';
+import e5 from './assets/Pirata (adulto) TS.jpg';
+import e6 from './assets/Pirata T4.jpg';
+import e7 from './assets/Pirata T5-8.jpg';
+import e8 from './assets/Pirata TS (2).jpg';
+import e9 from './assets/Pirata TS.jpg';
+import f1 from './assets/Rapunzel T6-8.jpg';
+import f2 from './assets/Rapunzel T8-10.jpg';
+import f3 from './assets/Santi Claus T4-6.jpg';
+import f4 from './assets/Sevillana T5-6.jpg';
+import f5 from './assets/Sevillana T8.jpg';
+import f6 from './assets/Sirenita T5.jpg';
+import f7 from './assets/Smoquin T8-9.jpg';
+import f8 from './assets/Sofia T3-5.jpg';
+import f9 from './assets/Sofia T6 (con accesorios).jpg';
+/* ==================================================== */
+import g1 from './assets/Sombrero de Mariachi.jpg';
+import g2 from './assets/Spidergirl TS.jpg';
+import g3 from './assets/Spiderman T6.jpg';
+import g4 from './assets/Tradicional Chinita T6-8.jpg';
+import g5 from './assets/Vampiresa y Bruja.jpg';
+
+export const costumes = [
+    {name: 'Vampiresa y Bruja', img: g5, fitSize: '0', gender: 'F'},
+    {name: 'Tradicional China', img: g4, fitSize: '6-8', gender: 'F'},
+    {name: 'Spiderman', img: g3, fitSize: '6', gender: 'M'},
+    {name: 'Spidergirl', img: g2, fitSize: 'S', gender: 'F'},
+    {name: 'Sombrero Mariachi', img: g1, fitSize: '0', gender: 'M'},
+    {name: 'Sofia (con accesorios)', img: f9, fitSize: '6', gender: 'F'},
+    {name: 'Sofia', img: f8, fitSize: '3-5', gender: 'F'},
+    {name: 'Smoquin', img: f7, fitSize: '8-9', gender: 'M'},
+    {name: 'Sirenita', img: f6, fitSize: '5', gender: 'F'},
+    {name: 'Sevillana', img: f5, fitSize: '8', gender: 'F'},
+    {name: 'Sevillana', img: f4, fitSize: '5-6', gender: 'F'},
+    {name: 'Santa Claus', img: f3, fitSize: '4-6', gender: 'M'},
+    {name: 'Rapunzel', img: f2, fitSize: '8-10', gender: 'F'},
+    {name: 'Rapunzel', img: f1, fitSize: '6-8', gender: 'F'},
+    {name: 'Pirata', img: e9, fitSize: 'S', gender: 'F'},
+    {name: 'Pirata', img: e8, fitSize: 'S', gender: 'F'},
+    {name: 'Pirata', img: e7, fitSize: '5-8', gender: 'F'},
+    {name: 'Pirata', img: e6, fitSize: '4', gender: 'F'},
+    {name: 'Pirata', img: e5, fitSize: 'Adulto S', gender: 'F'},
+    {name: 'Pilar (Jose Marti)', img: e4, fitSize: '0', gender: 'F'},
+    {name: 'Picapiedras', img: e3, fitSize: 'Adulto', gender: 'M'},
+    {name: 'Peter Pan', img: e2, fitSize: '0', gender: 'M'},
+    {name: 'Payasito', img: e1, fitSize: '5-7', gender: 'M'},
+    {name: 'Nemo (cabeza)', img: d9, fitSize: '0', gender: 'M'},
+    {name: 'Murcielago', img: d8, fitSize: '6-8', gender: 'F'},
+    {name: 'Moda de los 70', img: d7, fitSize: '0', gender: 'F'},
+    {name: 'Mickey Mouse', img: d6, fitSize: 'Adulto', gender: 'M'},
+    {name: 'Menique', img: d5, fitSize: '4-6', gender: 'M'},
+    {name: 'Mariachi', img: d4, fitSize: '0', gender: 'M'},
+    {name: 'Malefica', img: d3, fitSize: 'S', gender: 'F'},
+    {name: 'Ladybug', img: d2, fitSize: '4', gender: 'F'},
+    {name: 'It', img: d1, fitSize: 'Adulto', gender: 'M'},
+    {name: 'Halloween', img: c9, fitSize: 'Adulto', gender: 'F'},
+    {name: 'Gata', img: c8, fitSize: 'Adulto', gender: 'F'},
+    {name: 'Fiona', img: c7, fitSize: '8-10', gender: 'F'},
+    {name: 'Bruja (con sombrero)', img: c6, fitSize: 0, gender: 'F'},
+    {name: 'Elena de Avalor', img: c5, fitSize: '8', gender: 'F'},
+    {name: 'Egipcio', img: c4, fitSize: 'Adulto', gender: 'M'},
+    {name: 'Cleopatra', img: c3, fitSize: 'Adulto', gender: 'F'},
+    {name: 'Capitan America', img: c2, fitSize: 8, gender: 'M'},
+    {name: 'Cangrejo (cabeza)', img: c1, fitSize: 0, gender: 'M'},
+    {name: 'Cabaret', img: b9, fitSize: 0, gender: 'F'},
+    {name: 'Buddy el Vaquero', img: b8, fitSize: '5-6', gender: 'M'},
+    {name: 'Brujita Verde', img: b7, fitSize: 0, gender: 'F'},
+    {name: 'Brujita', img: b6, fitSize: '10', gender: 'F'},
+    {name: 'Brujita', img: b5, fitSize: '4-5', gender: 'F'},
+    {name: 'Bruja', img: b4, fitSize: S, gender: 'F'},
+    {name: 'Bruja', img: b3, fitSize: S, gender: 'F'},
+    {name: 'Antifaz', img: a1, fitSize: 0, gender: 'F'},
+    {name: 'Antifaz', img: a2, fitSize: 0, gender: 'F'},
+    {name: 'Barbie en Paris', img: a3, fitSize: 10, gender: 'F'},
+    {name: 'Bella (con tiara)', img: a4, fitSize: 0, gender: 'F'},
+    {name: 'Bella', img: a5, fitSize: 10, gender: 'F'},
+    {name: 'Blanca Nieves', img: a6, fitSize: '5-6', gender: 'F'},
+    {name: 'Bombero', img: a7, fitSize: '4-5', gender: 'M'},
+    {name: 'Bruja', img: a8, fitSize: 'Adulto', gender: 'F'},
+    {name: 'Bruja', img: a9, fitSize: 'Adulto', gender: 'F'},
+    {name: 'Bruja', img: b1, fitSize: 'Adulto', gender: 'F'},
+    {name: 'Bruja', img: b2, fitSize: 'Adulto', gender: 'F'},
+];
